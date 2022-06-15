c8d8a6ed0ec1e2f6a1284fd633da8ee8c9c01e51
José Luis Tanicuchí Cadena <66490180+Josluistanic@users.noreply.github.com>
2022-06-15 16:26:33 -0500

    CRUD completo terminad con ejemplo de llamada AJAX para eliminar un registro

--- controllers/consulta.php
+++ controllers/consulta.php
@@ -1,6 +1,8 @@
 <?php
-class Consulta extends Controller{
-    function __construct(){
+class Consulta extends Controller
+{
+    function __construct()
+    {
         parent::__construct();
         $alumnos = [];
     }
@@ -8,24 +10,26 @@ class Consulta extends Controller{
      * The function render() is called from the controller, which calls the model to get the data, and
      * then calls the view to display the data
      */
-    function render() {
+    function render()
+    {
         $alumnos = $this->model->get();
         $this->view->alumnos = $alumnos;
         $this->view->render('consulta/index');
     }
-    
-    function readAlumno($param = null) {
+
+    function readAlumno($param = null)
+    {
         $matricula = $param[0];
         $alumno = $this->model->getAlumnnoByMatricula($matricula);
-        
+
         session_start();
         $_SESSION['matriculaAlumno'] = $alumno->matricula;
 
         $this->view->alumno = $alumno;
         $this->view->render('consulta/detalle');
-
     }
-    function updateAlumno(){
+    function updateAlumno()
+    {
         session_start();
         $matricula    = $_SESSION['matriculaAlumno'];
         $nombre       = $_POST['nombre'];
@@ -42,26 +46,22 @@ class Consulta extends Controller{
 
             $this->view->alumno = $alumno;
             $this->view->mensaje = 'Se ha actualizado el alumno';
-        }else{
+        } else {
             $this->view->mensaje = 'Error al actualizar el alumno';
         }
         $this->view->render('consulta/detalle');
-
     }
-    function deleteAlumno($param = null) {
+    function deleteAlumno($param = null)
+    {
         $matricula = $param[0];
 
-        
-        /*
         if ($this->model->delete($matricula)) {
-            $this->view->mensaje = 'Se ha eliminado el alumno';
-        }else{
-            $this->view->mensaje = 'Error al eliminar el alumno';
+            $mensaje = 'Se ha eliminado el alumno';
+        } else {
+            $mensaje = 'Error al eliminar el alumno';
         }
+        //$this->render();
 
-        $this->render();
-        */
+        echo $mensaje ;
     }
 }
-
-?>
--- views/consulta/index.php
+++ views/consulta/index.php
@@ -5,13 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main</title>
+
 </head>
 <body>
     <?php require 'views/header.php' ?>
 
 
     <div id="main">
-        <h1>Esta es la vista de consulta</h1>
+        <h1 class="center">Esta es la vista de consulta</h1>
+        <div id="respuesta" class="center"></div>
         <table class="w-100">
             <thead>
                 <tr>
@@ -20,15 +22,15 @@
                     <th>Apellido</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody id="tbody-alumnos">
                 <?php foreach ($this->alumnos as $alumno) :?>
-                <tr>
+                <tr id="fila-<?= $alumno->matricula; ?>">
                     <td><?=  $alumno->matricula;    ?></td>
                     <td><?=  $alumno->nombre;       ?></td>
                     <td><?=  $alumno->apellido;     ?></td>
-                    <td><button>Eliminar</button></td>
                     <td><a href="<?= filter_var(constant('URL').'consulta/readAlumno/'.$alumno->matricula,FILTER_SANITIZE_URL); ?>">Editar</a></td>
-                    <td><a href="<?= filter_var(constant('URL').'consulta/deleteAlumno/'.$alumno->matricula,FILTER_SANITIZE_URL); ?>">Eliminar</a></td>
+                    <!-- <td><a href="<?= filter_var(constant('URL').'consulta/deleteAlumno/'.$alumno->matricula,FILTER_SANITIZE_URL); ?>">Eliminar</a></td> -->
+                    <td><button class="bEliminar" data-matricula="<?= $alumno->matricula; ?>"'>Eliminar</button></td>
                 </tr>
                 <?php endforeach; ?>
             </tbody>
@@ -37,5 +39,7 @@
 
 
     <?php require 'views/footer.php' ?>
+
+    <script src="<?= filter_var(constant('URL').'public/js/main.js',FILTER_SANITIZE_URL); ?>"></script>
 </body>
 </html>
